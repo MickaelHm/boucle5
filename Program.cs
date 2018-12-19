ff137e74ae86edac98c2e86748d446f1535fa5e5
MickaelHm <harmeau.mickael@gmail.com>
2018-12-19 11:59:26 +0100

    complet

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace boucle5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] week = new string[] { "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche" };
+            int indexer;
+            for (indexer = 0; indexer < 7; indexer++)
+            {
+                Console.WriteLine(week[indexer]);
+            }
+        }
+    }
+}
